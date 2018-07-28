4bd643b83b14bacf48140053fd370385dd2cb7fe
Ashish Varshney <ashishvarshney2k16@gmail.com>
2018-07-29 00:12:48 +0530

    Add files via upload
    send email through exe file in python using cs_Freeze module

---
+++ emailsetup.py
@@ -0,0 +1,6 @@
+from cx_Freeze import setup, Executable
+setup(name="e-mail",
+	version="0.1",
+	description="you can send email with this programme",
+	executables=[Executable(r"/home/lol/Desktop/python programme/emailt.py")]
+	)
