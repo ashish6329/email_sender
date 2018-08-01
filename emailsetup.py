173533faa1cc9248a06fbf861c31439b61db32fb
Ashish Varshney <ashishvarshney2k16@gmail.com>
2018-08-01 22:44:00 +0530

    Add files via upload
    Desktop icon make using this programme

--- emailsetup.py
+++ emailsetup.py
@@ -1,6 +1,18 @@
 from cx_Freeze import setup, Executable
-setup(name="e-mail",
-	version="0.1",
+setup(name="e-mail",#name anything
+	version="2.0",
+	author="Ashish varshney",
 	description="you can send email with this programme",
-	executables=[Executable(r"/home/lol/Desktop/python programme/emailt.py")]
+	executables=[Executable(r"/home/lol/Desktop/python programme/emailt.py",
+							icon=r"/home/lol/Desktop/python programme/icon.ico",
+							shortcutName="send Mail",
+							shortcutDir="DesktopFolder")] 
 	)
+
+
+
+
+#for windows use command on terminal python filename.py bdist_msi
+# for tar file python filename.py bdist_tar
+#for mac python filename.py bdist_dumb
+
